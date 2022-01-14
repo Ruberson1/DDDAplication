85edcbd3feeb6a6d0d5cee75dd337f89608ef47f
Ruberson <Ruberson.as@gmail.com>
2022-01-14 12:45:03 -0300

    primeiro commit

---
+++ RestApiModeloDDD.Application/ApplicationServiceCliente.cs
@@ -0,0 +1,40 @@
+﻿using RestApiModeloDDD.Application.Dtos;
+using RestApiModeloDDD.Application.Interfaces;
+using RestApiModeloDDD.Application.Interfaces.Mappers;
+using RestApiModeloDDD.Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.Application
+{
+    public class ApplicationServiceCliente : IApplicationServiceCliente
+    {
+        private readonly IServiceCliente serviceCliente;
+        private readonly IMapperCliente mapperCliente;
+
+        public void Add(ClienteDto clienteDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClienteDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteDto GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ClienteDto clienteDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ClienteDto clienteDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
